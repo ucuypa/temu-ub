2971ef931c5cff6bf3c9bbba215e24ea6278860f
Ahua <ahua.iqomaddin@gmail.com>
2025-05-12 08:50:38 +0700

    edit profile feature

--- temu-ub/app/Http/Requests/ProfileUpdateRequest.php
+++ temu-ub/app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,10 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'phone' => 'nullable|numeric|max:20',
+            'bio' => 'nullable|string|max:255',
+            'password' => 'nullable|string|min:8',
+            'photo' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
         ];
     }
 }
